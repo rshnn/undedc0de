4eda01259f3277e65050bbfc479a491eca9cc57e
Roshan <roshanpatel06@gmail.com>
2017-01-27 07:45:53 -0500

    added assembly file from ilab machine

---
+++ assmebly-files/undead_code_ilab.s
@@ -0,0 +1,60 @@
+	.file	"undead_code.c"
+	.section	.rodata
+.LC0:
+	.string	"I am slain!"
+	.text
+	.globl	segment_fault_handler
+	.type	segment_fault_handler, @function
+segment_fault_handler:
+.LFB2:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	movl	$.LC0, (%esp)
+	call	puts
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	segment_fault_handler, .-segment_fault_handler
+	.section	.rodata
+.LC1:
+	.string	"I live again!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB3:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+	movl	$0, 28(%esp)
+	movl	$segment_fault_handler, 4(%esp)
+	movl	$11, (%esp)
+	call	signal
+	movl	$0, %eax
+	movl	(%eax), %eax
+	movl	%eax, 28(%esp)
+	movl	$.LC1, (%esp)
+	call	puts
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-4)"
+	.section	.note.GNU-stack,"",@progbits
