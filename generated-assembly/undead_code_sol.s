50cedd1b12d950b8e87b569ceac92432ae6a8e65
rshnn-mutalisk <roshanpatel06@gmail.com>
2017-01-27 08:27:15 -0500

    added new assembly outputs

---
+++ generated-assembly/undead_code_sol.s
@@ -0,0 +1,68 @@
+	.file	"undead_code.c"
+	.section	.rodata
+.LC0:
+	.string	"I am slain!"
+	.text
+	.globl	segment_fault_handler
+	.type	segment_fault_handler, @function
+segment_fault_handler:
+.LFB2:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$40, %esp
+	movl	$.LC0, (%esp)
+	call	puts
+	leal	8(%ebp), %eax
+	movl	%eax, -12(%ebp)
+	addl	$60, -12(%ebp)
+	movl	-12(%ebp), %eax
+	movl	(%eax), %eax
+	leal	6(%eax), %edx
+	movl	-12(%ebp), %eax
+	movl	%edx, (%eax)
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	segment_fault_handler, .-segment_fault_handler
+	.section	.rodata
+.LC1:
+	.string	"I live again!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB3:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+	movl	$0, 28(%esp)
+	movl	$segment_fault_handler, 4(%esp)
+	movl	$11, (%esp)
+	call	signal
+	movl	$0, %eax
+	movl	(%eax), %eax
+	movl	%eax, 28(%esp)
+	movl	$.LC1, (%esp)
+	call	puts
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE3:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.5-4ubuntu2) 4.8.5"
+	.section	.note.GNU-stack,"",@progbits
--- readme.md
+++ readme.md
@@ -1,3 +1,25 @@
 # CS518 Assignment 0 
 ### undead_code 
 
+
+
+### dev-notes
+
++ makefile created
+    + make commands available 
+        - make all
+        - make debug
+        - make assembly
+        - make clean
+    * Compiling with no optimizations and in 32bit
+        - -m32 -O0 
+    * Compiles to assembly with -S flag 
+
++ The assembly file outputs are exactly the same between the ilab -S compile and mutalisk -S compile 
+    * 1 diff
+        -   .ident  "GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-4)"
+        -   .ident  "GCC: (Ubuntu 4.8.5-4ubuntu2) 4.8.5"
+    * All else is same
+    * Compiling with debugger flag (-g) will add some .nonsense stuff to the assembly 
+        - Doesn't actually functionally change anything, just makes it cluttered 
+
